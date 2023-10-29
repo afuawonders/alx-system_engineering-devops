8a22fa4522ebf9a56731c5d6ba265a4b7d9614ab
afuawonders <afuawonders@gmail.com>
2023-10-28 17:11:56 -0700

    ssh key

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,30 @@
+# client configuration using puppet
+#include stdlib
+
+file_line { 'Turn off password authentication':
+
+  ensure  => present,
+
+  path    => '/etc/ssh/ssh_config',
+
+  line    => 'PasswordAuthentication no',
+
+  replace => true,
+
+}
+
+
+
+file_line { 'Declare identity file':
+
+  ensure  => present,
+
+  path    => '/etc/ssh/ssh_config',
+
+  line    => 'IdentityFile ~/.ssh/school',
+
+  replace => true,
+
+}
+
+
