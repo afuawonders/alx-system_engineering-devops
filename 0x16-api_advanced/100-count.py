c702614c653abcc5c57effaf483abbc7084753b4
afuawonders <afuawonders@gmail.com>
2023-12-12 16:35:51 +0000

    advanced api

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,42 +1,55 @@
-import requests
+#!/usr/bin/python3
+"""
+parses the title of all hot articles, and prints a sorted count of given
+keywords (case-insensitive, delimited by spaces)
+"""
+from collections import OrderedDict
+from requests import get
 
-def count_words(subreddit, word_list, after=None, results=None):
-    if results is None:
-        results = {}
 
-    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
-    params = {"limit": 100, "after": after}
-
+def count_words(subreddit, word_list, after=None, match_dict={}):
+    """parses the title of all hot articles, and prints a sorted count of given
+    keywords (case-insensitive, delimited by spaces)
+    Args:
+        subreddit (str): subreddit
+        word_list (list): list of words to count occurrences for
+        after (str): refer to next page
+        match_dict (dict): dictionary of frequency of words from word_list
+        flag (int): indicate when match_dict is complete
+    """
     try:
-        response = requests.get(f"https://www.reddit.com/r/{subreddit}/hot.json", headers=headers, params=params, allow_redirects=False)
-        data = response.json().get("data", {})
-        children = data.get("children", [])
+        r = get('https://www.reddit.com/r/{}/hot.json?limit=100&&'
+                'after={}'.format(subreddit, after),
+                headers={'User-Agent': 'bc'})
+        sub_dict = r.json()
 
-        for child in children:
-            title = child.get("data", {}).get("title", "").lower()
+        if match_dict == {}:
+            for w in word_list:
+                match_dict[w] = 0
 
-            for keyword in word_list:
-                keyword_lower = keyword.lower()
-                if keyword_lower in title:
-                    results[keyword_lower] = results.get(keyword_lower, 0) + title.count(keyword_lower)
+        after = sub_dict['data']['after']
 
-        if data.get("after") is not None:
-            count_words(subreddit, word_list, after=data["after"], results=results)
+        for i in range(len(sub_dict['data']['children'])):
+            title_string = sub_dict['data']['children'][i]['data']['title']
+            search_list = title_string.split()
+            for word in search_list:
+                for w in word_list:
+                    if w.lower() == word.lower():
+                        match_dict[w] += 1
+
+        if after is None:
+            descend_dict = OrderedDict(sorted(match_dict.items(),
+                                              key=lambda x: x[1],
+                                              reverse=True))
+            zero_count = 0
+            for k, v in descend_dict.items():
+                if v != 0:
+                    print("{}: {}".format(k, v))
+                else:
+                    zero_count += 1
+            if zero_count == len(descend_dict):
+                print()
         else:
-            sorted_results = sorted(results.items(), key=lambda x: (-x[1], x[0]))
-
-            for keyword, count in sorted_results:
-                print(f"{keyword}: {count}")
-
-    except Exception as e:
-        pass  # Handle exceptions if needed (e.g., invalid subreddit)
-
-if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) < 3:
-        print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
-        print("Ex: {} programming 'python java javascript'".format(sys.argv[0]))
-    else:
-        count_words(sys.argv[1], [x for x in sys.argv[2].split()])
-
+            count_words(subreddit, word_list, after, match_dict)
+    except:
+        pass
